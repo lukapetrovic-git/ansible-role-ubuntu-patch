8a9e15f0d02a68986b589a783e0f880644bc66ec
lukapetrovic-git <lukapetrovicguitar@gmail.com>
2024-08-20 14:09:47 +0200

    initial code commit

---
+++ files/get_upgraded_packages.py
@@ -0,0 +1,31 @@
+import argparse
+import re
+
+arguments = argparse.ArgumentParser()
+arguments.add_argument('-p','--apt-history-path', nargs=1, dest='apt_history_path', help='Path to the APT history log file', required=True) #, default='/var/log/apt/history.log')
+arguments.add_argument('-u','--upgrade-string', nargs=1, dest='upgrade_string', help='String to filter on to filter upgraded packages', required=True) #, default='Upgrade: ')
+arguments = arguments.parse_args()
+
+def get_last_string_occurence(file_path: str, search_string: str) -> str:
+  last_occurrence = None
+  with open(file_path, 'r') as file:
+      for line in file:
+          if search_string in line:
+              last_occurrence = line.strip()
+  return last_occurrence
+
+upgraded_packages_string = (get_last_string_occurence(arguments.apt_history_path[0],arguments.upgrade_string[0])).removeprefix(arguments.upgrade_string[0])
+
+# split the string on commas outside parentheses
+upgraded_array = re.split(r',\s*(?![^()]*\))', upgraded_packages_string)
+
+# extract the package name and the upgraded version from the parentheses
+upgraded_packages_w_versions = []
+for part in upgraded_array:
+    match = re.match(r'([^:]+):[^()]+\(([^,]+),\s*([^,]+)\)', part)
+    if match:
+        package_name = match.group(1).strip()
+        version = match.group(3).strip()
+        upgraded_packages_w_versions.append(f"{package_name}={version}")
+
+print(upgraded_packages_w_versions)
